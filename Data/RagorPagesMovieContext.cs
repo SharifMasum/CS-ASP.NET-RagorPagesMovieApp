867ad62d7976fb3df32a4e813f582d768a2b5336
Shariful Islam <shariful.islam.mec@gmail.com>
2022-10-29 00:55:26 +0300

    Add project files.

---
+++ Data/RagorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RagorPagesMovie.Models;
+
+namespace RagorPagesMovie.Data
+{
+    public class RagorPagesMovieContext : DbContext
+    {
+        public RagorPagesMovieContext (DbContextOptions<RagorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RagorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
